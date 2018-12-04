977c91a4b15072bb7dc56e58fabfc03fe5b799de
Spooore <erward.erward@gmail.com>
2018-12-04 14:25:08 +0100

    First vue front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,41 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App';
+import router from './router';
+import 'es6-promise/auto'; // for Promise polyfill
+import { getField, updateField } from 'vuex-map-fields';
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css';
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.use(SuiVue);
+
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+    state: {
+        form: {
+            storeType: '',
+            storeDetail: '',
+            fullName: '',
+            firstName: '',
+            lastName: '',
+            role: null,
+            joinDate: '',
+            isLocatedInVic: null,
+            locationInVic: '',
+        },
+    },
+    getters: {
+        getField,
+    },
+    mutations: {
+        updateField,
+    },
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    render: h => h(App),
+    router,
+    store,
+}).$mount('#app');
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,25 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Form1 from './components/Form1';
+import Form2 from './components/Form2';
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Form1,
+        },
+        {
+            path: '/form2',
+            component: Form2,
+        },
+    ]
+});
